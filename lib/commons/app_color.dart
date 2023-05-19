a5ee646a732cc583b0a5bfce7040a8d2097f7a52
m-maekakuchi <m.marikoromo@gmail.com>
2023-05-19 20:21:24 +0900

    home画面の静的実装の完了

--- lib/commons/app_color.dart
+++ lib/commons/app_color.dart
@@ -4,5 +4,9 @@ import 'package:flutter/material.dart';
 const textIconColor = Color.fromRGBO(95, 99, 104, 1);
 // テーマカラー
 const themeColor = Color.fromRGBO(205, 235, 195, 1.0);
+// Indicatorの最初の色
+const indicatorBeginColor = Color.fromRGBO(172, 221, 156, 1.0);
+//　Indicatorの最後の色
+const indicatorEndColor = Color.fromRGBO(65, 129, 44, 1.0);
 // 背景色
 const scaffoldBackgroundColor = Color.fromRGBO(248, 251, 242, 1);
