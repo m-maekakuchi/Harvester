f4eb208305d532ac02b295e92b4d20f753163f10
m-maekakuchi <m.marikoromo@gmail.com>
2023-06-08 06:22:24 +0900

    ナビゲーションバーの変更

---
+++ lib/commons/bottom_navigation_bar_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// ナビゲーションバーのアイテム
+const items = [
+  BottomNavigationBarItem(
+    icon: Icon(Icons.home_rounded),
+    label: 'ホーム',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.shopping_bag_rounded),
+    label: 'マイカード',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.photo_library_rounded),
+    label: '全カード',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.add_circle_rounded),
+    label: '追加',
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.photo_camera_back_rounded),
+    label: '写真',
+  ),
+];
