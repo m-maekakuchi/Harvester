f406a18b92906d05d0d36ca32dc99e6f40ae602e
m-maekakuchi <m.marikoromo@gmail.com>
2023-07-11 09:57:55 +0900

    Firebase Crashlyticsの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -23,6 +26,13 @@ Future<void> main() async{
   // );
   // Hiveの初期化
   await LocalStorageRepository().init();
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(ProviderScope(child: MyApp()),);
 }
 
